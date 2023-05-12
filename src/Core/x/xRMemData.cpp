0e19d6c4027df88f01eddf2c0b9d894b28cc2f00
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2023-05-11 22:32:10 -0500

    xRMemData.cpp done

---
+++ src/Core/x/xRMemData.cpp
@@ -0,0 +1,67 @@
+#include "xRMemData.h"
+
+#include "xMemMgr.h"
+
+#include <string.h>
+
+void* RyzMemData::operator new(size_t amt, S32 who, RyzMemGrow* growCtxt)
+{
+    void* mem = NULL;
+    S32 dogrow = 1;
+
+    if (!growCtxt) {
+        dogrow = 0;
+    } else if (!growCtxt->IsEnabled()) {
+        dogrow = 0;
+    }
+
+    if (dogrow) {
+        mem = xGROWALLOC(amt);
+    } else {
+        mem = xMALLOC(amt);
+    }
+
+    memset(mem, 0, 4);
+
+    return mem;
+}
+
+void RyzMemData::operator delete(void*)
+{
+}
+
+RyzMemGrow* RyzMemGrow::Init(xBase* growuser)
+{
+    if (ptr) return this;
+
+    amt_last = 0;
+    ptr_last = NULL;
+    user_last = NULL;
+    amt = 32;
+    ptr = (char*)xMALLOC(amt);
+    user = growuser;
+    flg_grow = 0x1;
+
+    return this;
+}
+
+RyzMemGrow* RyzMemGrow::Resume(xBase*)
+{
+    amt = amt_last;
+    ptr = ptr_last;
+    user = user_last;
+    flg_grow = 0x3;
+
+    return this;
+}
+
+void RyzMemGrow::Done()
+{
+    amt_last = amt;
+    ptr_last = ptr;
+    user_last = user;
+    amt = 0;
+    ptr = NULL;
+    user = NULL;
+    flg_grow = 0;
+}
--- src/Core/x/xRMemData.h
+++ src/Core/x/xRMemData.h
@@ -3,9 +3,12 @@
 #include "types.h"
 
 struct xBase;
+struct RyzMemGrow;
 
 struct RyzMemData
 {
+    void* operator new(size_t amt, S32 who, RyzMemGrow* growCtxt);
+    void operator delete(void*);
 };
 
 struct RyzMemGrow
@@ -18,4 +21,14 @@ protected:
     S32 amt_last;
     char* ptr_last;
     xBase* user_last;
+
+public:
+    RyzMemGrow* Init(xBase* growuser);
+    RyzMemGrow* Resume(xBase*);
+    void Done();
+
+    S32 IsEnabled()
+    {
+        return flg_grow & 0x1;
+    }
 };
