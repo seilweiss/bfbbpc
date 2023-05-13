286ec53a5fc8fc05bd4c8a50cc555cbd338d6281
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2023-05-12 21:09:43 -0500

    xBehaveGoalSimple.cpp done

--- src/Core/x/xBehaveGoalSimple.cpp
+++ src/Core/x/xBehaveGoalSimple.cpp
@@ -1,5 +1,73 @@
 #include "xBehaveGoalSimple.h"
 
-void xGoalSimple_RegisterTypes(xFactory* fac) WIP
+static xFactoryInst* GOALCreate_Generic(S32 who, RyzMemGrow* growCtxt, void*);
+static void GOALDestroy_Generic(xFactoryInst* item);
+
+void xGoalSimple_RegisterTypes(xFactory* fac)
 {
+    fac->RegItemType('GSM\0', GOALCreate_Generic, GOALDestroy_Generic);
+    fac->RegItemType('GSM\1', GOALCreate_Generic, GOALDestroy_Generic);
+}
+
+static xFactoryInst* GOALCreate_Generic(S32 who, RyzMemGrow* growCtxt, void*)
+{
+    xGoal* goal = NULL;
+
+    switch (who) {
+    case 'GSM\0':
+        goal = new (who, growCtxt) xGoalGeneric(who);
+        break;
+    case 'GSM\1':
+        goal = new (who, growCtxt) xGoalEmpty(who);
+        break;
+    }
+
+    return goal;
+}
+
+static void GOALDestroy_Generic(xFactoryInst* item)
+{
+    delete item;
+}
+
+S32 xGoalGeneric::Enter(F32 dt, void* updCtxt)
+{
+    if (fun_enter) {
+        return fun_enter(this, usrData, dt, updCtxt);
+    }
+
+    return xGoal::Enter(dt, updCtxt);
+}
+
+S32 xGoalGeneric::Exit(F32 dt, void* updCtxt)
+{
+    if (fun_exit) {
+        return fun_exit(this, usrData, dt, updCtxt);
+    }
+
+    return xGoal::Exit(dt, updCtxt);
+}
+
+S32 xGoalGeneric::Suspend(F32 dt, void* updCtxt)
+{
+    if (fun_suspend) {
+        return fun_suspend(this, usrData, dt, updCtxt);
+    }
+    return xGoal::Suspend(dt, updCtxt);
+}
+
+S32 xGoalGeneric::Resume(F32 dt, void* updCtxt)
+{
+    if (fun_resume) {
+        return fun_resume(this, usrData, dt, updCtxt);
+    }
+    return xGoal::Resume(dt, updCtxt);
+}
+
+S32 xGoalGeneric::SysEvent(xBase* from, xBase* to, U32 toEvent, const F32* toParam, xBase* toParamWidget, S32* handled)
+{
+    if (fun_sysevent) {
+        return fun_sysevent(this, usrData, from, to, toEvent, toParam, toParamWidget, handled);
+    }
+    return xGoal::SysEvent(from, to, toEvent, toParam, toParamWidget, handled);
 }
--- src/Core/x/xBehaveGoalSimple.h
+++ src/Core/x/xBehaveGoalSimple.h
@@ -2,4 +2,35 @@
 
 #include "xBehaviour.h"
 
+struct xGoalGeneric : xGoal
+{
+protected:
+    S32(*fun_enter)(xGoal*, void*, F32, void*);
+    S32(*fun_exit)(xGoal*, void*, F32, void*);
+    S32(*fun_suspend)(xGoal*, void*, F32, void*);
+    S32(*fun_resume)(xGoal*, void*, F32, void*);
+    S32(*fun_sysevent)(xGoal*, void*, xBase*, xBase*, U32, const F32*, xBase*, S32*);
+    void* usrData;
+
+public:
+    xGoalGeneric(S32 myType) : xGoal(myType) {}
+
+    const char* Name() { return "xGoalGeneric"; }
+    S32 Enter(F32 dt, void* updCtxt);
+    S32 Exit(F32 dt, void* updCtxt);
+    S32 Suspend(F32 dt, void* updCtxt);
+    S32 Resume(F32 dt, void* updCtxt);
+    S32 SysEvent(xBase* from, xBase* to, U32 toEvent, const F32* toParam, xBase* toParamWidget, S32* handled);
+    void Clear() {}
+};
+
+struct xGoalEmpty : xGoal
+{
+public:
+    xGoalEmpty(S32 myType) : xGoal(myType) {}
+
+    const char* Name() { return "xGoalEmpty"; }
+    void Clear() {}
+};
+
 void xGoalSimple_RegisterTypes(xFactory* fac);
